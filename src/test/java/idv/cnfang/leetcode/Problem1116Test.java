6804d74ee09bc77bac5825ac60e5833878bd7fde
Jenny Fang <fang1992119@gmail.com>
2019-11-11 12:25:36 +1100

    add test case

---
+++ src/test/java/idv/cnfang/leetcode/Problem1116Test.java
@@ -0,0 +1,14 @@
+package idv.cnfang.leetcode;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem1116Test {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
